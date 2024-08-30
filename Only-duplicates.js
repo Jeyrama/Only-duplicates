8602bec886d42b6af7b1e3caa051f8c46ab64658
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-30 11:59:43 -0700

    Update Only-duplicates.js

--- Only-duplicates.js
+++ Only-duplicates.js
@@ -9,3 +9,20 @@ Example:
 
 
 // Solution
+
+function onlyDuplicates(str) {
+  let arr = str.split("")
+   let newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		let count = 0;
+		for (let j = 0; j < arr.length; j++) {
+			if (arr[j] == arr[i]) {
+				count++;
+			}
+		}
+		if (count >= 2) {
+			newArr.push(arr[i]);
+		}
+	}
+  return newArr.join('');
+}
